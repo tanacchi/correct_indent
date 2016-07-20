1115e7ac04328e274104837cb327bfb3e70998d1
tanacchi <q111026d@mail.kyutech.jp>
2016-07-20 18:05:41 +0900

    add sourse code

---
+++ correct_indent.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#define MAX 1024
+
+void flat_string(char src[], char artifact[]);
+
+int main(void){
+
+  FILE *fp;
+
+  int input_buff;
+
+  char reader[MAX] = {0};
+  char scanner[MAX] = {0};
+  char output[MAX] = {0};
+
+  if((fp=fopen("work.c", "r")) != NULL){
+    int i;
+    for(i = 0; (input_buff = fgetc(fp)) != EOF; i++){
+      reader[i] = (char)input_buff;
+    }
+  }
+
+  printf("\n\nprintf = [%s]\n\n", reader);
+
+  flat_string(reader, scanner);
+
+  printf("\n\n[%s]\n\n", scanner);
+
+  return 0;
+ }
