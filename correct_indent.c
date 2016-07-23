6535979da30949db8007f5905b79b65924e64a0d
tanacchi <q111026d@mail.kyutech.jp>
2016-07-23 11:40:47 +0900

    just commit
    Please enter the commit message for your changes. Lines starting

--- correct_indent.c
+++ correct_indent.c
@@ -3,6 +3,8 @@
 #define MAX 1024
 
 void flat_string(char src[], char artifact[]);
+void replace_newline(char src[], char dist[]);
+void del_extra_space(char src[], char dist[]);
 
 int main(void){
 
@@ -14,7 +16,7 @@ int main(void){
   char scanner[MAX] = {0};
   char output[MAX] = {0};
 
-  if((fp=fopen("work.c", "r")) != NULL){
+  if((fp=fopen("C:\\Users\\bata\\Desktop\\work.c", "r")) != NULL){
     int i;
     for(i = 0; (input_buff = fgetc(fp)) != EOF; i++){
       reader[i] = (char)input_buff;
@@ -23,9 +25,39 @@ int main(void){
 
   printf("\n\nprintf = [%s]\n\n", reader);
 
-  flat_string(reader, scanner);
+  //flat_string(reader, scanner);
+  replace_newline(reader, scanner);
+ 
 
   printf("\n\n[%s]\n\n", scanner);
 
   return 0;
  }
+
+void flat_string(char src[], char artifact[]){
+  int i;
+  for (i = 0; src[i] != '\0'; i++){
+    if (src[i] == '\n') artifact[i] = ' ';
+    else artifact[i] = src[i];
+  }
+}
+
+void replace_newline(char src[], char dist[]){
+  int i;
+  for (i = 0; src[i] != NULL; i++){
+    if(src[i] = '\n') dist[i] = ' ';
+    else dist[i] = src[i];
+  }
+  dist[i+1] = '\0';
+}
+void del_extra_space(char src[], char dist[]){
+  int i, j = 0;
+  for (i = 0; src[i] != NULL; i++){
+    if ((src[i] = ' ') && (src[i+1] = ' ')) ;
+    else {
+      dist[j] = src[i];
+      j++;    
+    }
+    dist[j+1] = ' ';
+  }
+}
