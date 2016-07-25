67585fd0d376057fa2aae5b8d93e6ad32361a398
tanacchi <q111026d@mail.kyutech.jp>
2016-07-25 17:31:06 +0900

    nothing to do

--- correct_indent.c
+++ correct_indent.c
@@ -27,33 +27,27 @@ int main(void){
 
   //flat_string(reader, scanner);
   replace_newline(reader, scanner);
- 
+  
+  del_extra_space(scanner, reader); 
 
-  printf("\n\n[%s]\n\n", scanner);
+  printf("\n\n[%s]\n\n", reader);
 
   return 0;
  }
 
-void flat_string(char src[], char artifact[]){
-  int i;
-  for (i = 0; src[i] != '\0'; i++){
-    if (src[i] == '\n') artifact[i] = ' ';
-    else artifact[i] = src[i];
-  }
-}
-
 void replace_newline(char src[], char dist[]){
   int i;
-  for (i = 0; src[i] != NULL; i++){
-    if(src[i] = '\n') dist[i] = ' ';
+  for (i = 0; src[i] != '\0'; i++){
+    if(src[i] == '\n') dist[i] = ' ';
     else dist[i] = src[i];
   }
   dist[i+1] = '\0';
 }
+
 void del_extra_space(char src[], char dist[]){
   int i, j = 0;
-  for (i = 0; src[i] != NULL; i++){
-    if ((src[i] = ' ') && (src[i+1] = ' ')) ;
+  for (i = 0; src[i] != '\0'; i++){
+    if ((src[i] == ' ') && (src[i+1] = ' ')) ;
     else {
       dist[j] = src[i];
       j++;    
