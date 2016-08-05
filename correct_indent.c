35b9199de243f312b8de3722a8d0b4d4b954f6d7
U-DESKTOP-VI3ELN4\bata <tanacchi>
2016-08-05 14:56:08 +0900

    input src to ary

--- correct_indent.c
+++ correct_indent.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 
-#define MAX 1024
+#define MAX 2048
 
 void flat_string(char src[], char artifact[]);
 void replace_newline(char src[], char dist[]);
 void del_extra_space(char src[], char dist[]);
+int countHeadSpace(char* src);
+int countTaleSpace(char* src, int length);
+void split_string_space(char* src, char** dist);
+int find_next_space(char* src);
 
 int main(int argc, char* argv[]){
 
   FILE *fp;
 
+  int i;
   int input_buff;
 
   char reader[MAX] = {0};
   char scanner[MAX] = {0};
-  char output[MAX] = {0};
+  char* output[MAX] = {0};
 
   if((fp=fopen(argv[1], "r")) != NULL){
-    int i;
     for(i = 0; (input_buff = fgetc(fp)) != EOF; i++){
       reader[i] = (char)input_buff;
     }
@@ -35,6 +39,12 @@ int main(int argc, char* argv[]){
 
   printf("\n\n[%s]\n\n", reader);
 
+  split_string_space(reader, output);
+
+
+  for (i = 0; output[i] != NULL; i++)
+    printf("%s\n", output[i]);
+
   return 0;
  }
 
@@ -47,14 +57,50 @@ void replace_newline(char src[], char dist[]){
   dist[i+1] = '\0';
 }
 
+int countHeadSpace(char* src) {
+  int count;
+  for (count = 0; src[count] == ' '; count++);
+  return count;
+}
+
+int countTaleSpace(char* src, int length){
+  int i, count;
+  //  for (i = length - 1; src[i] == ' '; i--) count++;
+  //  return count;
+  for (i = length -1; src[i] == ' '; i--);
+  return length - 1 - i;
+}
+
 void del_extra_space(char src[], char dist[]){
   int i, j = 0;
-  for (i = 0; src[i] != '\0'; i++){
-    if ((src[i] == ' ') && (src[i+1] = ' ')) ;
-    else {
+  const int head_space_count = countHeadSpace(src);
+  for (i = head_space_count; src[i] != '\0'; i++){
+    if ((src[i] != ' ') || (src[i+1] != ' ')){ 
       dist[j] = src[i];
       j++;
     }
-    dist[j+1] = '\0';
   }
+  const int tale_space_count = countTaleSpace(dist, j);
+  dist[j - tale_space_count] = '\0';
+}
+
+void split_string_space(char* src, char** dist){
+  int i;
+  int offset = 0;
+  int brank;
+
+  for (i = 0; (brank = find_next_space(src + offset)) != -1; i++){
+    dist[i] = src + offset;
+    offset += brank + 1;
+    dist[i][brank] = '\0';
+  }
+  dist[i] = src + offset;
+  dist[i+1] = NULL;
+}
+
+int find_next_space(char* src){
+  int count;
+  for (count = 0; src[count] != ' '; count++)
+    if (src[count] == '\0') return -1;
+  return count;
 }
