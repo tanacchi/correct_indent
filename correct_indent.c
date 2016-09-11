c8e1e24c0c180039e369008e78015dfeb9e8107a
tanacchi <q111026d@mail.kyutech.jp>
2016-09-11 15:53:29 +0900

    Add replace_tab
    finish...

--- correct_indent.c
+++ correct_indent.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAX 8000
+#define MAX 10000
 
 void flat_string(char src[], char artifact[]);
 void replace_newline(char src[], char dest[]);
+void replace_tab(char src[], char dest[]);
 void del_extra_space(char src[], char dest[]);
 int count_head_space(char* src);
 int count_tale_space(char* src, int length);
@@ -34,7 +35,9 @@ int main(int argc, char* argv[]){
   int input_buff;
   
   char reader[MAX] = {0};
-  char scanner[MAX] = {0};
+  char newline_none[MAX] = {0};
+  char tab_none[MAX] = {0};
+  char scanned[MAX] = {0};
   char* array[MAX] = {0};
   
   if((fp=fopen(argv[1], "r")) != NULL){
@@ -45,21 +48,23 @@ int main(int argc, char* argv[]){
 
   /* printf("\n\n[%s]\n\n", reader); */
   
-  replace_newline(reader, scanner);
+  replace_newline(reader, newline_none);
 
-  /* printf("\n\n[%s]\n\n", scanner); */
+  replace_tab(newline_none, tab_none);
   
-  del_extra_space(scanner, reader);
+  /* printf("\n\n[%s]\n\n", scanner); */
 
-  /*printf("\n\n[%s]\n\n", reader);  */
+  del_extra_space(tab_none, scanned);
 
-  split_string_space(reader, array);
+  /* printf("\n\n[%s]\n\n", reader); */
+
+  split_string_space(scanned, array);
 
 
-  /* for (i = 0; array[i] != '\0'; i++){ */
-  /*   printf("%s\n", array[i]); */
-  /* } */
-  /* putchar('\n'); */
+  for (i = 0; array[i] != '\0'; i++){
+    printf("%s\n", array[i]);
+  }
+  putchar('\n');
 
   
   /* for (i = 0; array[i] != '\0'; i++){ */
@@ -85,37 +90,53 @@ int main(int argc, char* argv[]){
   return 0;
  }
 
-/* ------------------  Replace all newline to a space  ---------------- */
 void replace_newline(char src[], char dest[]){
   int i;
   for (i = 0; src[i] != '\0'; i++){
-    if(src[i] == '\n') dest[i] = ' ';
+    if (src[i] == '\n') dest[i] = ' ';
     else dest[i] = src[i];
   }
   dest[i+1] = '\0';
 }
 
-/* -----------------  Count spaces in front of "#include"  ----------- */
+void replace_tab(char src[], char dest[]){
+  int i;
+  for (i = 0; src[i] != '\0'; i++){
+    if (src[i] == '\t') dest[i] = ' ';
+    else dest[i] = src[i];
+  }
+  dest[i+1] = '\0';
+}
+
 int count_head_space(char* src) {
   int count;
   for (count = 0; src[count] == ' '; count++);
   return count;
 }
 
-/* ----------------  Count spaces behind "return }" -------------------*/
 int count_tale_space(char* src, int length){
   int i, count;
   for (i = length -1; src[i] == ' '; i--);
   return length - 1 - i;
 }
 
-/*-----------------  Skip consecutive spaces ------------------------- */
 void del_extra_space(char src[], char dest[]){
   int i, j = 0;
   const int head_space_count = count_head_space(src);
   for (i = head_space_count; src[i] != '\0'; i++){
-    if ((src[i] != ' ') || (src[i+1] != ' ')){ 
+    /* if ((src[i] != ' ') || (src[i+1] != ' ')){ */
+    /*   dest[j] = src[i]; */
+    /*   j++; */
+    /* } */
+    if (src[i] != ' '){
       dest[j] = src[i];
+      j++;
+    }
+    else {
+      dest[j] = src[i];
+      j++;
+      while(src[i] == ' ') i++;
+      dest[j]= src[i];
       j++;
     }
   }
@@ -123,7 +144,6 @@ void del_extra_space(char src[], char dest[]){
   dest[j - tale_space_count] = '\0';
 }
 
-/*-----  Sprit source and put into a two-dimentional array  -----------*/
 void split_string_space(char* src, char** dest){
   int i;
   int offset = 0;
@@ -138,7 +158,6 @@ void split_string_space(char* src, char** dest){
   dest[i+1] = NULL;
 }
 
-/*-----------------  Find the next space to split  -------------------*/
 int find_next_space(char* src){
   int count;
   for (count = 0; src[count] != ' '; count++)
@@ -146,7 +165,6 @@ int find_next_space(char* src){
   return count;
 }
 
-/*----------------  Function to out put ------  */
 void output_vol1(char* src[]){
   int i;
   int level = 0;
@@ -195,10 +213,10 @@ void output_vol1(char* src[]){
 	    i++;
 	  }
 	}
-	while (!check_tale_char(src[i], ')')){
-	  printf("%s ", src[i]);
-	  i++;
-	}
+	/* while (!check_tale_char(src[i], '')){ */
+	/*   printf("%s ", src[i]); */
+	/*   i++; */
+	/* } */
 	printf("%s", src[i]);
       }
       else if (!strncmp(src[i], "printf", 6)){
@@ -253,13 +271,12 @@ void output_vol2(char* src, int paren_gap, int bracket_gap){
 
 void output_vol3(char** src){
   int i, j;
-  int paren_gap, bracket_gap;
+  int paren_gap = 0, bracket_gap = 0;
   
   for (i = 0; src[i] != NULL; i++){
-    
     paren_gap = count_char(src, i, '(') - count_char(src, i, ')');
     bracket_gap = count_char(src, i, '{') - count_char(src, i, '}');
-    
+  
     if (paren_gap){
       printf("%s ",src[i]);
     }
@@ -278,7 +295,12 @@ void output_vol3(char** src){
       }
       else if(check_tale_char(src[i], '}')){
 	printf("%s\n", src[i]);
-	put_indent_level(bracket_gap);
+	if (check_tale_char(src[i+1], '}')){
+	  put_indent_level(bracket_gap - 1);
+	}
+	else{
+	  put_indent_level(bracket_gap);
+	}
       }
       else if (check_tale_char(src[i], ';')){
 	printf("%s\n", src[i]);
@@ -292,37 +314,38 @@ void output_vol3(char** src){
       else{
 	printf("%s ", src[i]);
       }
-    }    
+    }
+    
+    if ((paren_gap < 0) || (bracket_gap < 0)){
+      printf("\n\n[[ERROR!!]]\n\n");
+      break;
+    }
   }
 }
-/*------  Compare the tale of the string and specified charactor  -----  */
+
 int check_tale_char(char* src, char c){
   if (get_tale_char(src) == c) return 1;
   else return 0;
 }
 
-/*-------------  Tell the length of the string  -----------------------  */
 int get_string_length(char* src){
   char* start_point = src;
   while (*(src++) != '\0') ;
   return src - start_point -1;
 }
 
-/*---------  Another way to tell the length of the string  ------------  */
 int get_string_length_vol2(char* src){
   int i;
   for (i = 0; src[i] != '\0'; i++) ;
   return i;
 }
 
-/*---------  Return the tale charactor of the string  -------------------*/
 char get_tale_char(char* src){
   int length = get_string_length(src);
   if (length == 0) return '\0';
   return src[length - 1];
 }
 
-/*---------  Put indent level depending on "{}" in left of a line  -----*/
 void put_indent_level(int level){
   int i;
   for (i = 0; i < level; i++)
