1420a4787921329329558608117760a02dc487ef
tanacchi <q111026d@mail.kyutech.jp>
2017-05-07 18:00:02 +0900

    Add scanner
    失敗作の予感

--- correct_indent_vol2.c
+++ correct_indent_vol2.c
@@ -3,13 +3,37 @@
 
 #define MAX_WIDTH 100000
 
+typedef struct code_status_t{
+  int parenthesis_diff; 
+  int bracket_diff;
+  int curl_diff;
+  
+  int single_quotation_diff;
+  int double_quotation_diff; 
+
+  int encloce_comment_diff;
+  int slash_comment_flag;
+  
+  int indent_depth;
+  
+  char char_stock;
+  char prev_char;
+
+} CodeStatus;
+
 int count_head_space(const char*);
 int count_tale_space(const char*, const int);
-void read_souse_file(FILE* const fp, char* input_string);
-void remove_newline(char* input_string, char* newline_none);
-void remove_tab(char* newline_none, char* tab_none);
-void remove_extra_space(char* tab_none, char* space_less);
+int get_str_length(const char*);
+int check_status_error(CodeStatus);
+void set_indent(int);
 
+void read_souse_file(FILE* const, char*);
+void remove_newline(char*, char*);
+void remove_tab(char*, char*);
+void remove_extra_space(char*, char*);
+void set_status(const char*, CodeStatus*);
+void corrent_indent(const CodeStatus*, char*);
+  
 void read_souse_file(FILE* const fp, char* input_string) {
   int input_buff;
   int i;
@@ -17,7 +41,7 @@ void read_souse_file(FILE* const fp, char* input_string) {
     if (i > MAX_WIDTH - 1) { puts("Too large file !"); exit(-3); }
     input_string[i] = (char)input_buff;
   }
-  input_string[++i] = '\0';
+  input_string[i] = '\0';
 }
 
 void remove_newline(char* input_string, char* newline_none) {
@@ -36,7 +60,7 @@ void remove_tab(char* newline_none, char* tab_none) {
 
 void remove_extra_space(char* tab_none, char* space_less) {
   int i, j = 0;
-  static char copy_buff = '\n';
+  static char copy_buff = '\0';
   for (i = count_head_space(tab_none); tab_none[i] != '\0'; i++) {
     if (copy_buff == ' ' && tab_none[i] == ' ') ;
     else space_less[j++] = tab_none[i];
@@ -44,7 +68,49 @@ void remove_extra_space(char* tab_none, char* space_less) {
   }
   space_less[j - count_tale_space(space_less, j)] = '\0';
 }
- 
+
+void set_status(const char* space_less, CodeStatus* status) {
+  int i;
+  for (i = 0; space_less[i] != '\0'; i++) {
+    const char current_char = space_less[i];
+    if (status[i].prev_char != '\\') {
+      switch (current_char) {
+      case '\'': status[i].single_quotation_diff = ++status[i].single_quotation_diff % 2; break;
+      case '\"': status[i].double_quotation_diff = ++status[i].double_quotation_diff % 2; break;      
+      case '/' :
+        if (status[i].prev_char == '*') status[i].encloce_comment_diff--; 
+        if (status[i].prev_char == '/') status[i].slash_comment_flag = 1;      
+        break;
+      case '*' :
+        if (status[i].prev_char == '/') status[i].encloce_comment_diff++;
+        break;
+      }
+    }
+    if (!status[i].slash_comment_flag && !status[i].encloce_comment_diff/* && !status[i].single_quotation_diff && !status[i].double_quotation_diff*/) {
+      switch (current_char) {
+      case '(' : status[i].parenthesis_diff++; break;
+      case ')' : status[i].parenthesis_diff--; break;
+      case '[' : status[i].bracket_diff++; break;
+      case ']' : status[i].bracket_diff--; break;
+      case '{' : status[i].curl_diff++; break;
+      case '}' : status[i].curl_diff--; break;
+      }      
+    }
+    status[i].indent_depth = status[i].bracket_diff;
+    if (check_status_error(status[i])) { puts("There are some synthax error!"); exit(-4); }
+    status[i].prev_char = current_char;
+    status[i+1] = status[i];
+  }
+  status[i].prev_char = '\0';
+}
+
+void correct_indent(const CodeStatus* status, char* clean_code) {
+  int i;
+  for (i = 0; status[i].prev_char != '\0'; i++) {
+    clean_code[i] = status[i].prev_char;
+  }
+}
+
 int main(int argc, char** argv) {
   
   if (argc < 2) { puts("Please set a sourse file !"); return -1; }
@@ -64,8 +130,28 @@ int main(int argc, char** argv) {
   char space_less[MAX_WIDTH];
   remove_extra_space(tab_none, space_less);
 
-  puts(space_less);
+  const int length = get_str_length(space_less);
+  CodeStatus status_array[length];  
+  set_status(space_less, status_array);
+
+  char clean_code[MAX_WIDTH];
+  correct_indent(status_array, clean_code);
   
+  puts(clean_code);
+
+  printf("%d\n%d\n%d\n\n%d\n%d\n\n%d\n%d\n%c\n"
+
+         , status_array[length-1].parenthesis_diff
+         , status_array[length-1].bracket_diff
+         , status_array[length-1].curl_diff
+         
+         , status_array[length-1].single_quotation_diff
+         , status_array[length-1].double_quotation_diff
+
+         , status_array[length-1].slash_comment_flag
+         , status_array[length-1].encloce_comment_diff
+         , status_array[length-1].prev_char);
+
   return 0;
 }
 
@@ -79,4 +165,19 @@ int count_tale_space(const char* src, const int prev_tale) {
   int i = prev_tale;
   while (src[i] == ' ') i--;
   return prev_tale - i + 1;
+}
+
+int get_str_length(const char* src) {
+  int i;
+  for (i = 0; src[i] != '\0'; i++) ;
+  return i;
+}
+
+int check_status_error(CodeStatus status) {
+  return (status.parenthesis_diff < 0) || (status.bracket_diff < 0) || (status.curl_diff < 0) ;
+}
+
+void set_indent(int depth) {
+  int i;
+  for (i = 0; i < depth*2; i++) putchar(' ');
 }
