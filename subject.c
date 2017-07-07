ea846c2e69c99211c7b3f5431f66e405ad331de0
tanacchi <q111026d@mail.kyutech.jp>
2017-07-08 01:44:29 +0900

    Add suject file
    コンパイルが通るのが不思議なくらいグロい

---
+++ subject.c
@@ -0,0 +1,66 @@
+           
+
+
+                                  #include                                                              <stdio.h>
+
+
+
+#define                                                         MAX                                                                16
+
+int                                   
+                         
+                                     
+                                     
+                                     
+                                     
+                                     
+                                     
+                                     
+                                     
+                                 main() 
+                                        
+                                    {
+
+  int                                    i;
+for                                                                          (i  
+                                                                      = 0;                        
+                                                                              i <   
+                                                                                                    MAX;  
+       i++) 
+                                                                                        {                   
+                                                                printf("i = %d", 
+ i);
+    
+    
+    
+    if                                  
+ ((i
+                                                                                    % 
+                                                     
+                                                                     2) 
+                                                                     
+                                                                                       ==     0) 
+                                                                                       
+                                                                                       
+                                                                                       
+                                                                                       {
+   printf(" <- even !!");
+                                                                                         }
+    
+    
+    
+    
+    
+    
+    putchar('\n');                                                                      }  return 
+    
+    
+    
+ 0;
+
+
+
+
+
+
+}
