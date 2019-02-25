78e625143373abdd5a8ce1cfb2ff199feb3a5fbb
tanacchi <q111026d@mail.kyutech.jp>
2019-02-25 23:06:00 +0900

    Add structs of Attribute series

--- correct_indent_vol3/token.hpp
+++ correct_indent_vol3/token.hpp
@@ -11,5 +11,65 @@
 #include <string>
 #include <vector>
 #include <regex>
+#include <memory>
+
+struct Attribute {};
+
+struct Keyword :          private Attribute {};
+struct TypeKeyword :      private Keyword {};
+struct StatementKeyword : private Keyword {};
+
+struct Identifier :       private Attribute {};
+
+struct Literal :                  private Attribute {};
+struct CharLiteral :              private Literal {};
+struct StringLiteral :            private Literal {};
+struct NumberLiteral :            private Literal {};
+struct BinaryNumberLiteral :      private NumberLiteral {};
+struct OctalNumberLiteral :       private NumberLiteral {};
+struct DecimalNumberLiteral :     private NumberLiteral {};
+struct HexadecimalNumberLiteral : private NumberLiteral {};
+
+struct Space : private Attribute {};
+
+struct Simbol :    private Attribute {};
+struct Assign :    private Simbol {};
+struct Semicolon : private Simbol {};
+struct Comma :     private Simbol {};
+struct LParen :    private Simbol {};
+struct RParen :    private Simbol {};
+struct LBrace:     private Simbol {};
+struct RBrace:     private Simbol {};
+struct LBracket :  private Simbol {};
+struct RBracket :  private Simbol {};
+struct Comment:    private Simbol {};
+
+struct Operator :           private Simbol {};
+struct RelationalOperator : private Operator {};
+struct SubscriptOperator :  private Operator {};
+struct ArithmeticOperator : virtual private Operator {};
+struct LogicalOperator :    virtual private Operator {};
+struct BitwiseOperator :    virtual private Operator {};
+
+struct UnaryOperator :   virtual private  Operator {};
+struct BinaryOperator :  virtual private  Operator {};
+struct CompoundOperator: virtual private  Operator {};
+
+struct UnaryArithmeticOperator :    private UnaryOperator,    private ArithmeticOperator {};
+struct BinaryArithmeticOperator :   private BinaryOperator,   private ArithmeticOperator {};
+struct CompoundArithmeticOperator : private CompoundOperator, private ArithmeticOperator {};
+
+struct UnaryLogicalOperator :    private UnaryOperator,    private LogicalOperator {};
+struct BinaryLogicalOperator :   private BinaryOperator,   private LogicalOperator {};
+
+struct UnaryBitwiseOperator :   private UnaryOperator,    private BitwiseOperator {};
+struct BinaryBitwiseOperator :  private BinaryOperator,   private BitwiseOperator {};
+struct CompoundBitwiseOperator: private CompoundOperator, private BitwiseOperator {};
+
+struct Token
+{
+  std::unique_ptr<Attribute> attribute;
+  std::string                content;
+};
 
 #endif  // INCLUDED_TOKEN_HPP
