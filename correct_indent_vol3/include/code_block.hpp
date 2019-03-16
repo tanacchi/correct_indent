910b4133b3049705335ff8bce622ccbe4aa5c81d
tanacchi <q111026d@mail.kyutech.jp>
2019-03-16 23:03:43 +0900

    Modify to make code-block detector

--- correct_indent_vol3/include/code_block.hpp
+++ correct_indent_vol3/include/code_block.hpp
@@ -5,6 +5,7 @@
 #include <iterator>
 
 #define ATTR_RED   "\x1b[31m"
+#define ATTR_GREEN "\x1b[32m"
 #define ATTR_RESET "\x1b[0m"
 
 struct CodeBlock
@@ -49,14 +50,64 @@ inline bool is_end_symbol(std::string attr_name)
   return attr_name == "RParen" || attr_name == "RBrace";
 }
 
+token::TokenArray::iterator get_begin_itr(const token::TokenArray::iterator& end)
+{
+  std::string end_symbol{get_attr_name(end)};
+  std::string begin_symbol{end_symbol == "RParen" ? "LParen" : "LBrace"};
+  auto itr{end};
+  for (; get_attr_name(itr) != begin_symbol; --itr) ;
+  return itr;
+}
+
 using CodeBlockArray = std::vector<CodeBlock>;
 
 void gen_code_blocks(token::TokenArray&& tokens)
 {
   std::size_t current_level{};
-  for (auto itr{tokens.begin()}, end{tokens.end()}; itr != end; ++itr)
+  for (auto itr{tokens.begin()}; itr != tokens.end(); ++itr)
   {
+    std::cout << std::distance(itr, tokens.end()) << " ";
+    // std::cout << get_content(itr);
+    for (auto it{tokens.begin()}; it != tokens.end(); ++it)
+    {
+      if (it == itr)
+      {
+        std::cout << ATTR_GREEN << get_content(it) << ATTR_RESET;
+      }
+      else
+      {
+        std::cout << get_content(it);
+      }
+    }
+    std::cout << std::endl;
+    if (is_end_symbol(get_attr_name(itr)))
+    {
+
+      auto end_itr{std::next(itr)};
+      auto begin_itr{get_begin_itr(itr)};
+
+      for (auto it{begin_itr}; it != end_itr; ++it)
+      {
+        std::cout << ATTR_RED << get_content(it);
+      }
+      std::cout << ATTR_RESET << "level : " << current_level << std::endl;
+
+      tokens.erase(begin_itr, end_itr);
+      
+      itr = std::prev(begin_itr);
+
+      --current_level;
+    }
+    else if (is_begin_symbol(get_attr_name(itr)))
+    {
+      ++current_level;
+    }
+    else
+    {
+    }
+    std::cout << std::endl;
   }
+
 }
 
 #endif  // INCLUDED_CODE_BLOCK_HPP
