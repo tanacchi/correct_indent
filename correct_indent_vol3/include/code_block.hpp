f33d18be41729eb9eefeaeab0c6ab9622476094c
tanacchi <q111026d@mail.kyutech.jp>
2019-03-16 18:51:10 +0900

    Add CodeBlock visualizer

--- correct_indent_vol3/include/code_block.hpp
+++ correct_indent_vol3/include/code_block.hpp
@@ -14,23 +14,30 @@ struct CodeBlock
     Paren, Brace,
   };
 
-  static std::size_t current_level;
-
   CodeBlock(const token::TokenArray::iterator& tokens_begin, 
             const token::TokenArray::iterator& tokens_end,
-            Kind kind)
-    : level{current_level},
-      tokens{std::make_move_iterator(tokens_begin), std::make_move_iterator(tokens_end)},
-      kind{kind}
+            std::size_t level, std::string kind_str)
+    : begin{tokens_begin}, end{tokens_end},
+      level{level},
+      kind{kind_str == "Paren" ? Kind::Paren : Kind::Brace}
   {
   }
 
+  friend std::ostream& operator<<(std::ostream& os, const CodeBlock& code_block)
+  {
+    for (auto itr{code_block.begin}; itr != code_block.end; ++itr)
+    {
+      os << ATTR_RED << itr->token_ptr->content;
+    }
+    os << ATTR_RESET << " [ level : " << code_block.level << " ]"
+                     << " kind : " << (code_block.kind == CodeBlock::Kind::Paren ? "Paren" : "Brace") << std::endl;
+    return os;
+  }
+
+  token::TokenArray::iterator begin, end;
   std::size_t level;
-  token::TokenArray  tokens;
   Kind        kind;
 };
-
-std::size_t CodeBlock::current_level{0};
 
 inline bool is_begin_symbol(std::string attr_name)
 {
@@ -44,39 +51,11 @@ inline bool is_end_symbol(std::string attr_name)
 
 using CodeBlockArray = std::vector<CodeBlock>;
 
-token::TokenArray::iterator get_begin(const token::TokenArray::iterator& end)
-{
-  std::string end_symbol{get_attr_name(end)};
-  std::string begin_symbol{end_symbol == "RParen" ? "LParen" : "LBrace"};
-  auto itr{end};
-  for (; get_attr_name(itr) != begin_symbol; --itr) ;
-  return itr;
-}
-
 void gen_code_blocks(token::TokenArray&& tokens)
 {
+  std::size_t current_level{};
   for (auto itr{tokens.begin()}, end{tokens.end()}; itr != end; ++itr)
   {
-    if (is_begin_symbol(get_attr_name(itr)))
-    {
-      ++CodeBlock::current_level;
-    }
-    else if (is_end_symbol(get_attr_name(itr)))
-    {
-      auto end_itr{itr};
-      auto begin_itr{get_begin(itr)};
-      for (; begin_itr != end_itr; ++begin_itr)
-      {
-        std::cout << ATTR_RED << get_content(begin_itr);
-      }
-      std::cout << get_content(itr) << ATTR_RESET  << " [level : " << CodeBlock::current_level << " ]" << std::endl;
-      --CodeBlock::current_level;
-      tokens.erase(begin_itr, end_itr);
-    }
-    else 
-    {
-      std::cout << get_content(itr) << std::endl;
-    }
   }
 }
 
